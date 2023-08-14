d6a7f2f6b17681d16cb4c1b789d594671b943335
PMNYABUTO <pharminous@gmail.com>
2023-08-14 08:53:14 +0300

    add test class for both encryption and decryption

--- com/meroka/java/test/CipherTest.java
+++ com/meroka/java/test/CipherTest.java
@@ -6,7 +6,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CipherTest {
-    Cipher tets=new Cipher();
+    Cipher tests=new Cipher();
+
+    @Test
+    void confirmEncryption(){
+        String enString= Cipher.decrypt("VnARXYTjI",9);
+        assertEquals("MerIOPKaz",enString);
+    }
+
+    void confirmDecryption(){
+        String enString= Cipher.encrypt("MerIOPKaz",9);
+        assertEquals("VnARXYTjI",enString);
+    }
+
+
 
 
 }
